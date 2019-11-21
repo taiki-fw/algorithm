14095a8f3bcc4511a154e98a25c035207ebda340
taiki-fw <taiki_52@outlook.jp>
2019-11-21 21:14:48 +0900

    番兵方の処理内容が見れる関数を作成

--- search/linear-guard.ts
+++ search/linear-guard.ts
@@ -8,9 +8,22 @@ export function sqlSearchGuard(
   let i: number = 0;
   array[length] = key; //番兵
   for (i; i <= length; i++) {
+    showCalc(array, i);
     if (array[i] === key) {
       break;
     }
   }
   return i !== length ? i : -1;
 }
+
+// (現状の配列,現在調べているindex,操作回数)
+function showCalc(array: number[], index: number) {
+  let asterisk = new Array(array.length).fill(" ");
+  asterisk[index] = "*";
+  console.log(`  | ${asterisk.join(" ")}`);
+  console.log(` ${index}| ${array.join(" ")}`);
+  console.log("  |");
+}
+
+const sample = [1, 5, 7, 2, 7, 8, 5, 8, 9];
+sqlSearchGuard(sample, sample.length, 7);
